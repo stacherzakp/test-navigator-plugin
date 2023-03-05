2fc7df940d119e60eeafc61e33772ad682267511
Pawel Stacherzak <stachu@pawels-mbp.home>
2023-03-05 20:03:43 +0100

    Change icons to SVG

--- src/main/java/com/staszkox/test/navigator/icons/NavigationIcons.java
+++ src/main/java/com/staszkox/test/navigator/icons/NavigationIcons.java
@@ -5,9 +5,9 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 interface NavigationIcons {
-    Icon testClassAvailable = IconLoader.getIcon("/icons/greenTest.png", NavigationIcons.class);
-    Icon testClassNotAvailable = IconLoader.getIcon("/icons/redTest.png", NavigationIcons.class);
-    Icon noTestsInTestClass = IconLoader.getIcon("/icons/yellowTest.png", NavigationIcons.class);
-    Icon sourceClassAvailable = IconLoader.getIcon("/icons/greenSource.png", NavigationIcons.class);
-    Icon sourceClassNotAvailable = IconLoader.getIcon("/icons/redSource.png", NavigationIcons.class);
+    Icon testClassAvailable = IconLoader.getIcon("/icons/greenT.svg", NavigationIcons.class);
+    Icon testClassNotAvailable = IconLoader.getIcon("/icons/redT.svg", NavigationIcons.class);
+    Icon noTestsInTestClass = IconLoader.getIcon("/icons/yellowT.svg", NavigationIcons.class);
+    Icon sourceClassAvailable = IconLoader.getIcon("/icons/greenS.svg", NavigationIcons.class);
+    Icon sourceClassNotAvailable = IconLoader.getIcon("/icons/redS.svg", NavigationIcons.class);
 }
